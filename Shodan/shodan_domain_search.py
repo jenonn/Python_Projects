95a4dff2b39d229d1cd73335141e52b70a882494
jenonn <jeff.nonn@gmail.com>
2021-06-17 08:47:05 -0700

    Created Shodan domain search script

--- Shodan/shodan_domain_search.py
+++ Shodan/shodan_domain_search.py
@@ -1,1 +1,7 @@
 # Shodan domain search
+from shodan import shodan
+
+SHODAN_API_KEY = "{RwrUWVl9PHcBnCjBaUdrSaSNK7giXW2U}"
+api = shodan.Shodan(SHODAN_API_KEY)
+
+print(api.dns.domain_info(domain=input("Enter a domain name: "), history = False, type = None, page = 1))
